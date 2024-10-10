a1dedbb449a372c59d367b6d73644c17a35a2001
Figuur <lmulder_89@hotmail.nl>
2024-10-10 15:38:54 +0200

    4 kaarten

---
+++ script.js
@@ -0,0 +1,17 @@
+let wissel = document.getElementById("kaart1");
+let wissel2 = document.getElementById("kaart2");
+let wissel3 = document.getElementById("kaart3");
+let wissel4 = document.getElementById("kaart4");
+
+function kaart1() {
+    wissel.src = "afbeeldingen/geit.jpg";
+}
+function kaart2() {
+    wissel2.src = "afbeeldingen/geit.jpg";
+}
+function kaart3() {
+    wissel3.src = "afbeeldingen/possum.jpg";
+}
+function kaart4() {
+    wissel4.src = "afbeeldingen/possum.jpg";
+}
